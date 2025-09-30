72ee4068ee5646cfe52b4f96e8d3de7de597588d
karina751 <bravokarina51@gmail.com>
2025-09-30 22:07:53 +0000

    firebase config

--- mobileStart/src/config/firebaseConfig.js
+++ mobileStart/src/config/firebaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+ apiKey: "AIzaSyC4L8HtxWfyvnN_B5B3NxIZjBoCo-KqtkQ",
+  authDomain: "mobilestart-c0cc7.firebaseapp.com",
+  projectId: "mobilestart-c0cc7",
+  storageBucket: "mobilestart-c0cc7.firebasestorage.app",
+  messagingSenderId: "47873865647",
+  appId: "1:47873865647:web:5d656991e95ad8a4fb615a"
 };
 
 const app = initializeApp(firebaseConfig);
