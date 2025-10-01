d16126332ec005190f7166871e29243a447f8e3a
Cristianosky88 <cristian_skater_6@hotmail.com>
2025-10-01 20:20:50 -0300

    Cmabio de Color Background Home

--- TecnoSeguridad/screens/Home.js
+++ TecnoSeguridad/screens/Home.js
@@ -32,7 +32,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     padding: 20,
-    backgroundColor: '#3410c6ff',
+    backgroundColor: '#ffffffff',
   },
   logo: {
     width: 100,
